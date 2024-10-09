927c940493fd20b6b20cb260670d4635ab1e283c
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-09 01:49:53 -0300

    update triggers_users.sql

--- db/functions/functions_update_at.sql
+++ db/functions/functions_update_at.sql
@@ -1,7 +1,7 @@
 CREATE OR REPLACE FUNCTION update_timestamp()
 RETURNS TRIGGER AS $$
 BEGIN
-    NEW.update_at := CURRENT_TIMESTAMP;
+    NEW.update_at := NOW();
     RETURN NEW
 END
 $$ LANGUAGE plpgsql;
