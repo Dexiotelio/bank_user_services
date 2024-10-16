23cafdf26ca56c54db8a91de2b86fb2dd7b098f6
José Ignacio Herrera Gré <herreragrej@proton.me>
2024-10-15 23:26:10 -0300

    created customers.sql

---
+++ db/tables/customers.sql
@@ -0,0 +1,16 @@
+CREATE TABLE customers (
+    id BIGINT PRIMARY KEY GENERATED ALWAYS AS IDENTITY
+    first_name VARCHAR(100) NOT NULL,
+    last_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    password VARCHAR(255) NOT NULL,
+    phone VARCHAR(15) NOT NULL UNIQUE,
+    type_document VARCHAR(50) NOT NULL,
+    document_number VARCHAR(50) NOT NULL UNIQUE,
+    civil_status VARCHAR(50) NOT NULL,
+    address VARCHAR(100) NOT NULL,
+    nationality VARCHAR(100) NOT NULL,
+    date_born DATE,
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    update_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+)
